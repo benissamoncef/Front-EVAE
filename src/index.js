352b4f11aa2a5dd2f55fc5cf1fa947ba872c5142
benissamoncef <moncef.benissa@etu.uae.ac.ma>
2024-02-10 00:33:05 +0100

    Dépot de code source pour la première fois

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SignUp from './Composants/InscriptionEtudiant';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SignUp/>
+  </React.StrictMode>
+);
